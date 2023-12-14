d26bc3b9262c760253dfbb525bf9bd334647ca5e
justine lv <justine.lopesvicente@gmail.com>
2023-12-14 11:34:23 +0100

    add test

---
+++ tests/test_api.py
@@ -0,0 +1,24 @@
+from fastapi.testclient import TestClient
+from app import app
+from model import IrisSpecies
+
+client = TestClient(app)
+
+def test_predict_species():
+    input_data = {
+        "sepal_length": 5.1,
+        "sepal_width": 3.5,
+        "petal_length": 1.4,
+        "petal_width": 0.2
+    }
+
+    response = client.post('/predict', json=input_data)
+
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "prediction" in response_json
+    assert "probability" in response_json
+
+    assert isinstance(response_json["prediction"], str)
+    assert isinstance(response_json["probability"], float)
+    assert 0 <= response_json["probability"] <= 1
---
+++ tests/test_model.py
@@ -0,0 +1,33 @@
+import pytest
+from model import IrisModel
+
+def model_init(model_name):
+    return IrisModel(model_name)
+
+@pytest.fixture
+
+def test_valid_model_loading():
+    instance = model_init('iris_model.pkl')
+    assert instance.model is not None
+
+def test_invalid_model_loading():
+    with pytest.raises(FileNotFoundError):
+        model_init('none.pkl')
+
+def test_prediction():
+    sepal_length = 5.1
+    sepal_width = 3.5
+    petal_length = 1.4
+    petal_width = 0.2
+
+    species, probability = model_init('iris_model.pkl').predict_species(
+        sepal_length, sepal_width, petal_length, petal_width
+    )
+
+    assert isinstance(species, str)
+    assert isinstance(probability, float)
+    assert 0 <= probability <= 1
+
+def test_invalid_prediction_input():
+    with pytest.raises(ValueError):
+        model_init('iris_model.pkl').predict_species('a', 3.5, 1.4, 0.2)
